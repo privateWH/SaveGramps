1ae89ae1f0e530805b271479964f40b66a493584
Saad Sami <saad.b.sami@gmail.com>
2012-02-18 22:11:24 -0800

    updated velocity and added balltype property

--- SaveGramps/SaveGramps/GameObjects/Ball.cs
+++ SaveGramps/SaveGramps/GameObjects/Ball.cs
@@ -8,6 +8,12 @@ using GrandpaBrain;
 
 namespace SaveGramps.GameObjects
 {
+    enum BallType
+    {
+        Operand,
+        Number
+    }
+
     class Ball
     {
         public Vector2 position;
@@ -16,6 +22,8 @@ namespace SaveGramps.GameObjects
         public int xVelocityMultiplier;
         public static Texture2D Texture { get; set; }
         public Vector2 initialVelocity;
+        public BallType ballType { get; set; }
+
 
         private static Random randomNum = new Random();
 
@@ -29,13 +37,13 @@ namespace SaveGramps.GameObjects
             double y = Ball.randomNum.NextDouble();
             this.position = position;
             this.xVelocityMultiplier = xVelocityMultiplier;
-            this.initialVelocity = new Vector2((float)(x * 3.5), 9 + (float)(y * 3.5));
+            this.initialVelocity = new Vector2((float)(x * 2.5), 9 + (float)(y * 1.5));
         }
 
         public void Update(GameTime gameTime)
         {
             Console.WriteLine("time: " + gameTime.ElapsedGameTime.TotalSeconds);
-            this.position.Y = this.position.Y + (float)(-1 * initialVelocity.Y * time + 7.8 * time * time / 2);// + Ball.viewPort.Height - texture.Height;
+            this.position.Y = this.position.Y + (float)(-1 * initialVelocity.Y * time + 7.5 * time * time / 2);// + Ball.viewPort.Height - texture.Height;
             this.position.X = this.position.X + (float)(xVelocityMultiplier * initialVelocity.X * time);
             this.time = this.time + gameTime.ElapsedGameTime.TotalSeconds;
         }
@@ -72,6 +80,7 @@ namespace SaveGramps.GameObjects
         public OperatorBall(Operands value, Vector2 initialPosition, int xVelocityMultiplier)
             : base(initialPosition, xVelocityMultiplier)
         {
+            this.ballType = BallType.Operand;
             text = OperandHelper.ConvertOperandToString(value);
         }
     }
@@ -81,6 +90,7 @@ namespace SaveGramps.GameObjects
         public NumberBall(Int32 value, Vector2 initialPosition, int xVelocityMultiplier)
             : base(initialPosition, xVelocityMultiplier)
         {
+            this.ballType = BallType.Operand;
             text = value.ToString();
         }
 
