4e2ca5d04892dd5a55a4debe715065153c188d2b
Weinian He <weinianhe@hotmail.com>
2012-02-18 18:04:44 -0800

    update answer.

--- GrandpaBrain/Answer.cs
+++ GrandpaBrain/Answer.cs
@@ -7,9 +7,10 @@ namespace GrandpaBrain
 {
     public class Answer
     {
-        private Response expectedResponse;
-        private Response userResponse;
-        private bool isDirty = false; 
+        private Response expectedResponse; // need to wire it up with the generator
+        private Response userResponse = new Response();
+        private bool isDirty = false;
+        private int? result = null;
         public void AddNumber(int number)
         {
             isDirty = true;
@@ -22,53 +23,97 @@ namespace GrandpaBrain
             userResponse.Operands.Add(op);
         }
 
-        public bool IsCorrect()
+        private bool IsCorrect()
         {
-            int? result = ComputeResponse();
+            if (isDirty)
+            {
+                result = ComputeResponse(userResponse.Numbers,userResponse.Operands);
+                isDirty = false;
+            }
             return (result.HasValue) ? result.Value == expectedResponse.Answer : false ;
         }
 
         public bool ShouldTerminate(out string message){
+            //Calls IsCorrect or GotPotential
+            // Normal Termination: IsCorrect() == true
+            // Abnormal Termination: GotPotential == false, meaning no possible combination in the selection to finish the game.
             message = string.Empty;
-            throw new NotImplementedException();
-            return false;
+            if (!IsCorrect())
+            {
+                if (GotPotential()) return false;
+                message = "NO POTENTIAL";
+                return true;
+            }
+            return true;
         }
 
-        private bool GotPotential()
+        private bool GotPotentialHelper(IList<int> numInStack,IList<Operands> opInStack)
         {
-            if (isDirty)
-            {
 
-            }
+            IList<Operands> listOp = userResponse.Operands.ToList();
+            IList<Operands> remainOp = expectedResponse.Operands.SkipWhile(it => listOp.Contains(it)).ToList();
+
+            IList<int> listNum = userResponse.Numbers.ToList();
+            IList<int> remainNum = expectedResponse.Numbers.SkipWhile(it => listNum.Contains(it)).ToList();
 
             return true;
         }
 
-        private int? ComputeResponse()
+        private bool GotPotential()
         {
-            int numOps = userResponse.Operands.Count;
-            int numNum = userResponse.Numbers.Count;
+            //assuming IsCorrect = false or result == null;
+            // this implies we missing some operand(s) or number(s) to make the "potential" equation to have a result
+            IList<Operands> listOp = userResponse.Operands.ToList();
+            IList<Operands> remainOp = expectedResponse.Operands.SkipWhile(it => listOp.Contains(it)).ToList();
+
+            IList<int> listNum = userResponse.Numbers.ToList();
+            IList<int> remainNum = expectedResponse.Numbers.SkipWhile(it => listNum.Contains(it)).ToList();
+
+            int? result = ComputeResponse(listNum, listOp);
+            if (result.HasValue && result.Value == expectedResponse.Answer) return true;
+
+            foreach (var op in remainOp)
+            {
+                var newOps = listOp.ToList();
+                newOps.Add(op);
+                if (GotPotentialHelper(listNum, newOps)) return true;
+                foreach (var num in remainNum)
+                {
+                    var newNums = listNum.ToList();
+                    newNums.Add(num);
+                    if (GotPotentialHelper(newNums, newOps)) return true;
+                }
+            }
+            return false;
+        }
+
+        private int? ComputeResponse(IList<int> numbers, IList<Operands> ops)
+        {
+            int numOps = ops.Count;
+            int numNum = numbers.Count;
 
             if (numOps == numNum - 1 && numNum > 0 && numOps > 0)
             {
                 int i = 0;
-                int result = userResponse.Numbers[i];
-                foreach (var num in userResponse.Numbers)
+                int result = numbers[i];
+                while(i < numbers.Count)
                 {
-                    switch (userResponse.Operands[i++])
+                    switch (ops[i++])
                     {
                         case Operands.Add:
-                            result += userResponse.Numbers[i];
+                            result += numbers[i];
                             break;
                         case Operands.Divide:
-                            result /= userResponse.Numbers[i];
+                            result /= numbers[i];
                             break;
                         case Operands.Minus:
-                            result -= userResponse.Numbers[i];
+                            result -= numbers[i];
                             break;
                         case Operands.Times:
-                            result *= userResponse.Numbers[i];
+                            result *= numbers[i];
                             break;
+                        default:
+                            throw new Exception("Operand not implement");
                     }
                 }
                 return result;
