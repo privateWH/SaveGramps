0ecd6759e0e8aeb532a93917d0d7dfb590526cc4
Weinian He <weinianhe@hotmail.com>
2012-02-18 21:17:15 -0800

    modified code to work in the proper flow

--- GrandpaBrain/Answer.cs
+++ GrandpaBrain/Answer.cs
@@ -7,10 +7,19 @@ namespace GrandpaBrain
 {
     public class Answer
     {
+        private Generator answerGenerator;
         private Response expectedResponse; // need to wire it up with the generator
         private Response userResponse = new Response();
         private bool isDirty = false;
         private int? result = null;
+
+        private Answer(){ //internal test only
+        }
+        public Answer(Response expectedAnswer)
+        {
+            expectedResponse = expectedAnswer;
+        }
+
         public void AddNumber(int number)
         {
             isDirty = true;
@@ -47,47 +56,34 @@ namespace GrandpaBrain
             return true;
         }
 
-        private bool GotPotentialHelper(IList<int> numInStack,IList<Operands> opInStack)
+        private bool GotPotentialHelper(IList<int> listNum,IList<Operands> listOp)
         {
-
-            IList<Operands> listOp = userResponse.Operands.ToList();
-            IList<Operands> remainOp = expectedResponse.Operands.SkipWhile(it => listOp.Contains(it)).ToList();
-
-            IList<int> listNum = userResponse.Numbers.ToList();
             IList<int> remainNum = expectedResponse.Numbers.SkipWhile(it => listNum.Contains(it)).ToList();
-
-            return true;
+            IList<Operands> remainOp = expectedResponse.Operands.SkipWhile(it => listOp.Contains(it)).ToList();
+            int? result = ComputeResponse(listNum, listOp);
+            if (result.HasValue && result.Value == expectedResponse.Answer) return true;
+            foreach(var op in remainOp){
+                    var newOp = listOp.ToList();
+                    newOp.Add(op);
+                    if (GotPotentialHelper(listNum.ToList(), newOp)) return true;
+                    foreach (var num in remainNum)
+                    {
+                        var newNums = listNum.ToList();
+                        newNums.Add(num);
+                        if (GotPotentialHelper(newNums, newOp)) return true;
+                    }
+            }
+            return false;
         }
 
         private bool GotPotential()
         {
             //assuming IsCorrect = false or result == null;
             // this implies we missing some operand(s) or number(s) to make the "potential" equation to have a result
-            IList<Operands> listOp = userResponse.Operands.ToList();
-            IList<Operands> remainOp = expectedResponse.Operands.SkipWhile(it => listOp.Contains(it)).ToList();
-
-            IList<int> listNum = userResponse.Numbers.ToList();
-            IList<int> remainNum = expectedResponse.Numbers.SkipWhile(it => listNum.Contains(it)).ToList();
-
-            int? result = ComputeResponse(listNum, listOp);
-            if (result.HasValue && result.Value == expectedResponse.Answer) return true;
-
-            foreach (var op in remainOp)
-            {
-                var newOps = listOp.ToList();
-                newOps.Add(op);
-                if (GotPotentialHelper(listNum, newOps)) return true;
-                foreach (var num in remainNum)
-                {
-                    var newNums = listNum.ToList();
-                    newNums.Add(num);
-                    if (GotPotentialHelper(newNums, newOps)) return true;
-                }
-            }
-            return false;
+            return GotPotentialHelper(userResponse.Numbers, userResponse.Operands);
         }
 
-        private int? ComputeResponse(IList<int> numbers, IList<Operands> ops)
+        public static int? ComputeResponse(IList<int> numbers, IList<Operands> ops)
         {
             int numOps = ops.Count;
             int numNum = numbers.Count;
@@ -96,9 +92,10 @@ namespace GrandpaBrain
             {
                 int i = 0;
                 int result = numbers[i];
-                while(i < numbers.Count)
+
+                while(++i < numbers.Count)
                 {
-                    switch (ops[i++])
+                    switch (ops[i-1])
                     {
                         case Operands.Add:
                             result += numbers[i];
@@ -115,7 +112,7 @@ namespace GrandpaBrain
                         default:
                             throw new Exception("Operand not implement");
                     }
-                }
+                } 
                 return result;
             }
 
--- GrandpaBrain/Generator.cs
+++ GrandpaBrain/Generator.cs
@@ -15,27 +15,30 @@ namespace GrandpaBrain
         }
         public int CurrentLevel { get { return gen.current_lv; } }
         private int current_lv = 0;
-        private Dictionary<int, ILevel> lv;
-        static void RegisterLevel(ILevel l)
+        private Dictionary<int, ILevel> lv = new Dictionary<int,ILevel>();
+        public static int RegisterLevel(ILevel l)
         {
-            RegisterLevel(l, Int32.MinValue);
+            return RegisterLevel(l, Int32.MinValue);
         }
-        static void RegisterLevel(ILevel l, int lvs)
+        static int RegisterLevel(ILevel l, int lvs)
         {
+            gen.current_lv++;
             gen.current_lv = (lvs != Int32.MinValue) ? lvs : gen.current_lv;
-            gen.lv.Add(gen.current_lv++, l);
+            gen.lv.Add(gen.current_lv, l);
+            return gen.current_lv;
         }
-        public static Response Get(){
-		    return Get(gen.current_lv);
+        public static Response GetExpectedResponseByLevel()
+        {
+            return GetExpectedResponseByLevel(gen.current_lv);
 	    }
-    	private static Response Get(int lv){
+    	public static Response GetExpectedResponseByLevel(int lv){
 		    ILevel gameLv;
 		    if(gen.lv.TryGetValue(lv,out gameLv)){
-			return gameLv.Get(lv);
-		}
-		else{
+			    return gameLv.Get(lv);
+		    }
+		    else{
 		    throw new Exception("DEBUG: Cannot find Level in Number Generator");
-		}				
-	}
+		    }				
+	    }
     }
 }
